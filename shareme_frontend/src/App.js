afbeacc5192e2e62a247c90a263666992028aefc
Raymond Wang <hootf4@gmail.com>
2022-12-25 23:39:24 +0900

    feat: add assets

--- shareme_frontend/src/App.js
+++ shareme_frontend/src/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+
+import Login from './components/Login';
+import Home from './container/Home';
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello World.</h1>;
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/*" element={<Home />} />
+    </Routes>
+  );
 }
 
 export default App;
